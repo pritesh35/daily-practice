5ceb046e6cd09cc46c4d345cdb797b97eb5d1dd4
Pritesh gupta <139554350+pritesh35@users.noreply.github.com>
2026-09-14 20:24:19 +0530

    Sync Solution: Overlapping IntervalsSolved [Medium] (GFG)

---
+++ solutions/overlapping-intervals--170633/overlapping-intervals--170633.cpp
@@ -0,0 +1,33 @@
+class Solution {
+    public ArrayList<ArrayList<Integer>> mergeOverlap(int[][] arr) {
+        
+        // sorting the array
+        Arrays.sort(arr, (a,b) -> Integer.compare(a[0], b[0]));
+        
+        // logic to find overlap
+        
+        
+        ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+        int low = arr[0][0];
+        int high = arr[0][1];
+        
+        for(int i = 1; i < arr.length; i++){
+            
+            int a = arr[i][0];
+            int b = arr[i][1];
+            
+            if(a <= high){
+                low = Math.min(low, a);
+                high = Math.max(high, b);
+            }else{
+                list.add(new ArrayList<>(Arrays.asList(low, high)));
+                low = a;
+                high = b;
+            }
+            
+        }
+        list.add(new ArrayList<>(Arrays.asList(low, high)));
+        
+        return list;
+    }
+}
